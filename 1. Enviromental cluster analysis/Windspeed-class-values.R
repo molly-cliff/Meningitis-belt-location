f409887cb17ac9dc01277272a05c5d93a1bf6a5d
Molly Cliff <108263895+molly-cliff@users.noreply.github.com>
2024-04-16 09:49:02 +0100

    Update Windspeed-class-values.R

--- 1. Enviromental cluster analysis/Windspeed-class-values.R
+++ 1. Enviromental cluster analysis/Windspeed-class-values.R
@@ -6,21 +6,24 @@ library(terra)
 library(tiff)
 library(RStoolbox)
 library(sf)
+setwd("C:/Users/mvc32/OneDrive - University of Cambridge/Documents/Climate_meningitis_belt")
 
-
+#read in windspeed
 rastlist <- list.files(path = "windspeed", pattern='.nc$', all.files= T, full.names= T)
 allrasters <- stack(rastlist)
+#reduce resolution of file so that we are able to work with it
 allrasters<- aggregate(allrasters, fact=5, fun = mean)
 shape <-read_sf(dsn = ".", layer = "Shapefile_improved")
+#crop and mask aerosol raster data to africa ADMN2 shapefile
 files_stack <- crop(allrasters, shape)
-
-
-
 files_stack <- mask(files_stack, shape)
+
+#reduction of dimensionality of data
 rpc<-rasterPCA(files_stack)
 summary(rpc$model)
 raster_stack <- stack(rpc$map)
 
+# stack rasters which compromise 95% of raster data 
 test <- raster_stack[[1]]
 test2 <- raster_stack[[2]]
 test3 <- raster_stack[[3]]
@@ -34,18 +37,16 @@ test10 <- raster_stack[[10]]
 test11 <- raster_stack[[11]]
 test12 <- raster_stack[[12]]
 
-files_stack <- stack(test,test2,test3,test4,test5,test6,test7,test8,test9,test10,test11,test12)
-
-#                     
+files_stack <- stack(test,test2,test3,test4,test5,test6,test7,test8,test9,test10,test11,test12)                 
 allrasters<-files_stack                     
 
 
-
+#extract k means for africa, we will go on to test different numbers of clusters
 km<-as.matrix(allrasters)
 num_na <- sum(is.na(km))
 km[is.na(km)] <- mean(km, na.rm = TRUE)
 num_na <- sum(is.na(km))
-
+# Determine the optimal number of clusters using the elbow method
 wss <- numeric(20)
 for (i in 1:20) {
   kmeans_model <- kmeans(km, centers = i, nstart = 20)
@@ -53,9 +54,15 @@ for (i in 1:20) {
 }
 
 # Plot the elbow chart
-plot(1:20, wss, type = "b", pch = 14, frame = FALSE, xlab = "Number of Clusters (k)", ylab = "Total Within Sum of Squares")
+plot(1:20, wss, type = "b", pch = 19, frame = FALSE, xlab = "Number of Clusters (k)", ylab = "Total Within Sum of Squares")
+
+# Add a line for the potential elbow point
+#abline(v = which(diff(wss) == max(diff(wss))) + 1, col = "red", lty = 2)
+
 
 #set seed makes this reproducible as kmeans clustering can vary
+#conducts K-means clustering with 6 clusters on a dataset,employs hierarchical clustering
+#visualizes the resulting clusters on a raster map and displays the dendrogram from hierarchical clustering.
 set.seed(1)
 kmeans_result <- kmeans(km, centers = 6)
 cluster_labels <- kmeans_result$cluster
@@ -77,12 +84,12 @@ r_cluster <- mask(r_cluster, shape)
 plot(hc)
 
 
-#extracts most common cluster value for each district in africa, most common as 
+#extracts most common cluster value for each district in africa
 #opposed to average as clusters are distinct from each other)
 cl2test<-data.frame(shape,extract(r_cluster, shape, fun=modal, na.rm = TRUE))
 cl2test$zonaltest<-cl2test$extract.r_cluster..shape..fun...modal..na.rm...TRUE.
 plot(hc)
-#labelling zones, into the linked classes based on hc
+#hierachal clustering assign each cluster appropriately in order of hhow closely related they are based on dendorgram
 cl2test$zonalcat  <- with(cl2test, ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. == 1, 'Class 3', 
                                           ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. == 2, 'Class 1', 
                                                  ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE.== 3, 'Class 2',
@@ -90,15 +97,10 @@ cl2test$zonalcat  <- with(cl2test, ifelse(extract.r_cluster..shape..fun...modal.
                                                                ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. == 5, 'Class 4',
                                                                       ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. ==6 , 'Class 6',
                                                                              0 )))))))
-#why are there so many NAs? data quality? how to fill in
+#see how many nas there are 
 sum(is.na(cl2test$extract.r_cluster..shape..fun...modal..na.rm...TRUE.))
 table(cl2test$zonalcat)
-# here we should think about class distribution
-
-
-
-
-
+# get rid of islands
 cl2test <- subset(cl2test, COUNTRY != "Cabo Verde")
 cl2test <- subset(cl2test, COUNTRY != "Mauritius")
 cl2test <- subset(cl2test, COUNTRY != "Seychelles")
@@ -140,7 +142,7 @@ plot(hc)
 cl2test<-data.frame(shape,extract(r_cluster, shape, fun=modal, na.rm = TRUE))
 cl2test$zonaltest<-cl2test$extract.r_cluster..shape..fun...modal..na.rm...TRUE.
 plot(hc)
-#labelling zones, into the linked classes based on hc
+#hierachal clustering assign each cluster appropriately in order of hhow closely related they are based on dendorgram
 cl2test$zonalcat  <- with(cl2test, ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. == 1, 'Class 4', 
                                           ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. == 2, 'Class 5', 
                                                  ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE.== 3, 'Class 1',
@@ -149,12 +151,11 @@ cl2test$zonalcat  <- with(cl2test, ifelse(extract.r_cluster..shape..fun...modal.
                                                                       ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. ==6 , 'Class 3',
                                                                              ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. ==7 , 'Class 7',
                                                                              0 ))))))))
-#why are there so many NAs? data quality? how to fill in
+
+#see how many nas there are 
 sum(is.na(cl2test$extract.r_cluster..shape..fun...modal..na.rm...TRUE.))
 table(cl2test$zonalcat)
-# here we should think about class distribution
-
-
+# get rid of islands
 
 cl2test <- subset(cl2test, COUNTRY != "Cabo Verde")
 cl2test <- subset(cl2test, COUNTRY != "Mauritius")
@@ -191,15 +192,12 @@ plot(r_cluster)
 r_cluster <- mask(r_cluster, shape)
 plot(hc)
 
-
-
-
-#extracts most common cluster value for each district in africa, most common as 
+#extracts most common cluster value for each district in africa
 #opposed to average as clusters are distinct from each other)
 cl2test<-data.frame(shape,extract(r_cluster, shape, fun=modal, na.rm = TRUE))
 cl2test$zonaltest<-cl2test$extract.r_cluster..shape..fun...modal..na.rm...TRUE.
 plot(hc)
-#labelling zones, into the linked classes based on hc
+#hierachal clustering assign each cluster appropriately in order of hhow closely related they are based on dendorgram
 cl2test$zonalcat  <- with(cl2test, ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. == 1, 'Class 3', 
                                           ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. == 2, 'Class 6', 
                                                  ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE.== 3, 'Class 4',
@@ -210,13 +208,10 @@ cl2test$zonalcat  <- with(cl2test, ifelse(extract.r_cluster..shape..fun...modal.
                                                                                     ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. ==8 , 'Class 8',
                                                                                     0 )))))))))
 
-#why are there so many NAs? data quality? how to fill in
+#see how many nas there are 
 sum(is.na(cl2test$extract.r_cluster..shape..fun...modal..na.rm...TRUE.))
 table(cl2test$zonalcat)
-# here we should think about class distribution
-
-
-
+# get rid of islands
 cl2test <- subset(cl2test, COUNTRY != "Cabo Verde")
 cl2test <- subset(cl2test, COUNTRY != "Mauritius")
 cl2test <- subset(cl2test, COUNTRY != "Seychelles")
@@ -228,18 +223,14 @@ cl2testraster <- st_as_sf(cl2testraster)
 cl2testraster2<-cl2testraster[ , c('COUNTRY.x','NAME_1.x', 'GID_2','NAME_2.x','zonalcat')]
 total3test<-cl2testraster2[!duplicated(cl2testraster2[ , c("GID_2")]),]
 
-
-
+# Replace values in those rows with new values, we are replacing/editing classes with less than 50 districts
 rows_to_replace <- total3test$zonalcat %in% c("Class 1")
-
-# Replace values in those rows with new values
 total3test$zonalcat[rows_to_replace] <- "Class 2"
 st_write(total3test, "windspeedcluster8.shp",append=FALSE)
 
-#we are trying this with different cluster sizes, 10 clusters, through to 6
-#10 clusters
+
 #set seed makes this reproducible as kmeans clustering can vary
-set.seed(4320)
+set.seed(4)
 kmeans_result <- kmeans(km, centers = 9)
 cluster_labels <- kmeans_result$cluster
 similarity_matrix <- dist( kmeans_result$centers)
@@ -255,14 +246,12 @@ r_cluster <- mask(r_cluster, shape)
 plot(hc)
 
 
-
-#extracts most common cluster value for each district in africa, most common as 
-#opposed to average as clusters are distinct from each other
-#detach("package:R.utils", unload=TRUE)
+#extracts most common cluster value for each district in africa
+#opposed to average as clusters are distinct from each other)
 cl2test<-data.frame(shape,extract(r_cluster, shape, fun=modal, na.rm = TRUE))
 cl2test$zonaltest<-cl2test$extract.r_cluster..shape..fun...modal..na.rm...TRUE.
 plot(hc)
-#labelling zones, into the linked classes based on hc
+#hierachal clustering assign each cluster appropriately in order of hhow closely related they are based on dendorgram
 cl2test$zonalcat  <- with(cl2test, ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. == 1, 'Class 8', 
                                           ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. == 2, 'Class 5', 
                                                  ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE.== 3, 'Class 7',
@@ -272,17 +261,11 @@ cl2test$zonalcat  <- with(cl2test, ifelse(extract.r_cluster..shape..fun...modal.
                                                                              ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. ==7 , 'Class 9',
                                                                                     ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. == 8, 'Class 3',
                                                                                            ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. ==9 , 'Class 1',
-                                                                                                  
-                                                                                                         
-                                                                                                         
-                                                                                                         0))))))))))
-#why are there so many NAs? data quality? how to fill in
+                                                                                                   0))))))))))
+#see how many nas there are 
 sum(is.na(cl2test$extract.r_cluster..shape..fun...modal..na.rm...TRUE.))
 table(cl2test$zonalcat)
-
-
-
-
+# get rid of islands
 cl2test <- subset(cl2test, COUNTRY != "Cabo Verde")
 cl2test <- subset(cl2test, COUNTRY != "Mauritius")
 cl2test <- subset(cl2test, COUNTRY != "Seychelles")
@@ -290,39 +273,28 @@ cl2test <- subset(cl2test, COUNTRY != "São Tomé and Príncipe")
 cl2test <- subset(cl2test, COUNTRY != "Comoros")
 
 
-rows_with_missing <- cl2test[is.na(cl2test$zonalcat), ]
-
-
-# here we should think about class distribution
-
 cl2testraster <- merge(cl2test,shape,by="GID_2")
 cl2testraster <- st_as_sf(cl2testraster)
 cl2testraster2<-cl2testraster[ , c('COUNTRY.x','NAME_1.x', 'GID_2','NAME_2.x','zonalcat')]
 total3test<-cl2testraster2[!duplicated(cl2testraster2[ , c("GID_2")]),]
 
-
+# Replace values in those rows with new values, we are replacing/editing classes with less than 50 districts
 rows_to_replace <- total3test$zonalcat %in% c("Class 4")
-
-# Replace values in those rows with new values
 total3test$zonalcat[rows_to_replace] <- "Class 5"
-
 # Print the updated data frame
 print(df)
 
-
 # here we should think about class distribution
-st_write(total3test, "windspeedfixed.shp",append=FALSE)
+st_write(total3test, "windspeed9classes.shp",append=FALSE)
 
 
-#we are trying this with different cluster sizes, 10 clusters, through to 6
-#10 clusters
 #set seed makes this reproducible as kmeans clustering can vary
-set.seed(420)
-kmeans_result <- kmeans(km, centers = 9)
+set.seed(5)
+kmeans_result <- kmeans(km, centers = 10)
 cluster_labels <- kmeans_result$cluster
 similarity_matrix <- dist( kmeans_result$centers)
 hc <- hclust(similarity_matrix)
-reordered_labels <- cutree(hc,9) 
+reordered_labels <- cutree(hc,10) 
 cluster_labels <- reordered_labels[cluster_labels]
 cl = raster(files_stack)
 cl[] = cluster_labels 
@@ -332,15 +304,12 @@ plot(r_cluster)
 r_cluster <- mask(r_cluster, shape)
 plot(hc)
 
-
-
-#extracts most common cluster value for each district in africa, most common as 
-#opposed to average as clusters are distinct from each other
-#detach("package:R.utils", unload=TRUE)
+#extracts most common cluster value for each district in africa
+#opposed to average as clusters are distinct from each other)
 cl2test<-data.frame(shape,extract(r_cluster, shape, fun=modal, na.rm = TRUE))
 cl2test$zonaltest<-cl2test$extract.r_cluster..shape..fun...modal..na.rm...TRUE.
 plot(hc)
-#labelling zones, into the linked classes based on hc
+#hierachal clustering assign each cluster appropriately in order of hhow closely related they are based on dendorgram
 cl2test$zonalcat  <- with(cl2test, ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. == 1, 'Class 2', 
                                           ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. == 2, 'Class 4', 
                                                  ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE.== 3, 'Class 9',
@@ -351,38 +320,21 @@ cl2test$zonalcat  <- with(cl2test, ifelse(extract.r_cluster..shape..fun...modal.
                                                                                     ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. == 8, 'Class 3',
                                                                                            ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. ==9 , 'Class 5',
                                                                                                   ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. ==10 , 'Class 10',
-                                                                                                         
-                                                                                                         
-                                                                                                         0)))))))))))
-#why are there so many NAs? data quality? how to fill in
+                                                                                                     0)))))))))))
+#see how many nas there are 
 sum(is.na(cl2test$extract.r_cluster..shape..fun...modal..na.rm...TRUE.))
 table(cl2test$zonalcat)
-
-
-
-
-cl2test <- subset(cl2test, COUNTRY != "Cabo Verde")
-cl2test <- subset(cl2test, COUNTRY != "Mauritius")
-cl2test <- subset(cl2test, COUNTRY != "Seychelles")
-cl2test <- subset(cl2test, COUNTRY != "São Tomé and Príncipe")
-cl2test <- subset(cl2test, COUNTRY != "Comoros")
-
-
+# get rid of islands
 rows_with_missing <- cl2test[is.na(cl2test$zonalcat), ]
-
-
-# here we should think about class distribution
 
 cl2testraster <- merge(cl2test,shape,by="GID_2")
 cl2testraster <- st_as_sf(cl2testraster)
 cl2testraster2<-cl2testraster[ , c('COUNTRY.x','NAME_1.x', 'GID_2','NAME_2.x','zonalcat')]
-total3test<-cl2testraster2[!duplicated(cl2testraster2[ , c("GID_2")]),]
-# here we should think about class distribution
 st_write(total3test, "windspeed10clusters.shp",append=FALSE)
 
 
 #set seed makes this reproducible as kmeans clustering can vary
-set.seed(420)
+set.seed(5)
 kmeans_result <- kmeans(km, centers = 11)
 cluster_labels <- kmeans_result$cluster
 similarity_matrix <- dist( kmeans_result$centers)
@@ -399,13 +351,12 @@ plot(hc)
 
 
 
-#extracts most common cluster value for each district in africa, most common as 
-#opposed to average as clusters are distinct from each other
-#detach("package:R.utils", unload=TRUE)
+#extracts most common cluster value for each district in africa
+#opposed to average as clusters are distinct from each other)
 cl2test<-data.frame(shape,extract(r_cluster, shape, fun=modal, na.rm = TRUE))
 cl2test$zonaltest<-cl2test$extract.r_cluster..shape..fun...modal..na.rm...TRUE.
 plot(hc)
-#labelling zones, into the linked classes based on hc
+#hierachal clustering assign each cluster appropriately in order of hhow closely related they are based on dendorgram
 cl2test$zonalcat  <- with(cl2test, ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. == 1, 'Class 3', 
                                           ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. == 2, 'Class 7', 
                                                  ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE.== 3, 'Class 5',
@@ -419,33 +370,15 @@ cl2test$zonalcat  <- with(cl2test, ifelse(extract.r_cluster..shape..fun...modal.
                                                                                                          ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. ==11 , 'Class 2',
                                                                                                                 
                                                                                                                 0))))))))))))
-#why are there so many NAs? data quality? how to fill in
+#see how many nas there are 
 sum(is.na(cl2test$extract.r_cluster..shape..fun...modal..na.rm...TRUE.))
 table(cl2test$zonalcat)
-
-
-
-
-cl2test <- subset(cl2test, COUNTRY != "Cabo Verde")
-cl2test <- subset(cl2test, COUNTRY != "Mauritius")
-cl2test <- subset(cl2test, COUNTRY != "Seychelles")
-cl2test <- subset(cl2test, COUNTRY != "São Tomé and Príncipe")
-cl2test <- subset(cl2test, COUNTRY != "Comoros")
-
-
+# get rid of islands
 rows_with_missing <- cl2test[is.na(cl2test$zonalcat), ]
-
-# here we should think about class distribution
 
 cl2testraster <- merge(cl2test,shape,by="GID_2")
 cl2testraster <- st_as_sf(cl2testraster)
 cl2testraster2<-cl2testraster[ , c('COUNTRY.x','NAME_1.x', 'GID_2','NAME_2.x','zonalcat')]
 total3test<-cl2testraster2[!duplicated(cl2testraster2[ , c("GID_2")]),]
-# here we should think about class distribution
 st_write(total3test, "windspeed11clusters.shp",append=FALSE)
-
-
-
-
-
 
