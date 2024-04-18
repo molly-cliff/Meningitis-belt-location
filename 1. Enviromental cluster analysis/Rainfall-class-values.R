b6ba808362722a45cddb1dd648cf8d49aaa3efeb
Molly Cliff <108263895+molly-cliff@users.noreply.github.com>
2024-04-18 13:27:44 +0100

    Update Rainfall-class-values.R

--- 1. Enviromental cluster analysis/Rainfall-class-values.R
+++ 1. Enviromental cluster analysis/Rainfall-class-values.R
@@ -26,11 +26,11 @@ raster_stack <- stack(rpc$map)
 
 # Stacking the raster layers
 files_stack <- stack(raster_stack[[1]], raster_stack[[2]], raster_stack[[3]], raster_stack[[4]], raster_stack[[5]],
-                    raster_stack[[6]], raster_stack[[7]], raster_stack[[8]], raster_stack[[9]], raster_stack[[10]],
-                    raster_stack[[11]], raster_stack[[12]], raster_stack[[13]], raster_stack[[14]], raster_stack[[15]],
-                    raster_stack[[16]], raster_stack[[17]], raster_stack[[18]], raster_stack[[19]], raster_stack[[20]],
-                    raster_stack[[21]], raster_stack[[22]], raster_stack[[23]], raster_stack[[24]], raster_stack[[25]],
-                    raster_stack[[26]])
+                     raster_stack[[6]], raster_stack[[7]], raster_stack[[8]], raster_stack[[9]], raster_stack[[10]],
+                     raster_stack[[11]], raster_stack[[12]], raster_stack[[13]], raster_stack[[14]], raster_stack[[15]],
+                     raster_stack[[16]], raster_stack[[17]], raster_stack[[18]], raster_stack[[19]], raster_stack[[20]],
+                     raster_stack[[21]], raster_stack[[22]], raster_stack[[23]], raster_stack[[24]], raster_stack[[25]],
+                     raster_stack[[26]])
 
 # Masking the stacked raster layers
 files_stack <- mask(files_stack, shape)
@@ -49,31 +49,33 @@ for (i in 1:20) {
 # Plot the elbow chart
 plot(1:20, wss, type = "b", pch = 19, frame = FALSE, xlab = "Number of Clusters (k)", ylab = "Total Within Sum of Squares")
 
-set.seed(1)
 # Perform K-means clustering with 6 centers
+set.seed(42)
 kmeans_result <- kmeans(km, centers = 6)
 cluster_labels <- kmeans_result$cluster
-
-# Calculate similarity matrix and perform hierarchical clustering
-similarity_matrix <- dist(kmeans_result$centers)
+original_labels <- kmeans_result$cluster
+comparison <- original_labels == cluster_labels
+matching_percentage <- sum(comparison) / length(comparison) * 100
+# Determine similarity matrix using hierarchical clustering
+similarity_matrix <- dist( kmeans_result$centers)
 hc <- hclust(similarity_matrix)
-reordered_labels <- cutree(hc, 6) 
+reordered_labels <- cutree(hc,6) 
 cluster_labels <- reordered_labels[cluster_labels]
-
 # Assign cluster labels to raster
-cl <- raster(files_stack)
+cl = raster(allrasters)
 cl[] = cluster_labels 
-
-# Plot the clustered raster
-plot(cl)
-
-# Mask the raster to Africa shape
-r_cluster <- mask(cl, shape)
 plot(hc)
 
-# Extract the most common cluster value for each district in Africa
-cl2test <- data.frame(shape, extract(r_cluster, shape, fun = modal, na.rm = TRUE))
-#hierachal clustering assign each cluster appropriately in order of hhow closely related they are based on dendorgram
+r_cluster <- setValues(cl, cluster_labels)
+plot(r_cluster)
+r_cluster <- mask(r_cluster, shape)
+plot(hc)
+
+# Extract most common cluster value for each district in Africa
+cl2test<-data.frame(shape,extract(r_cluster, shape, fun=modal, na.rm = TRUE))
+cl2test$zonaltest<-cl2test$extract.r_cluster..shape..fun...modal..na.rm...TRUE.
+plot(hc)
+# Assign hierarchical clustering labels
 cl2test$zonalcat  <- with(cl2test, ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. == 1, 'Class 2', 
                                           ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. == 2, 'Class 3', 
                                                  ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE.== 3, 'Class 4',
@@ -81,51 +83,56 @@ cl2test$zonalcat  <- with(cl2test, ifelse(extract.r_cluster..shape..fun...modal.
                                                                ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. == 5, 'Class 6',
                                                                       ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. ==6 , 'Class 1',
                          0 )))))))
-# Check for missing values
-sum(is.na(cl2test$extract.r_cluster..shape..fun...modal..na.rm...TRUE.))
-
-# Remove islands
-cl2test <- subset(cl2test, !(COUNTRY %in% c("Cabo Verde", "Mauritius", "Seychelles", "São Tomé and Príncipe", "Comoros")))
-
-# Convert to sf object
-cl2test_missing <- st_as_sf(cl2test)
+# Check for NA values
 sum(is.na(cl2test$extract.r_cluster..shape..fun...modal..na.rm...TRUE.))
 table(cl2test$zonalcat)
 
-# Merge shapefile and aerosol data
-cl2testraster <- merge(cl2test, shape, by = "GID_2")
-cl2testraster2 <- cl2testraster[, c('COUNTRY.x','NAME_1.x', 'GID_2','NAME_2.x','zonalcat')]
-total3test <- cl2testraster2[!duplicated(cl2testraster2[, "GID_2"]), ]
-st_write(total3test, "rainfall6clusers.shp",append=FALSE)
 
 
-set.seed(2)
+# Remove islands
+cl2test <- subset(cl2test, COUNTRY != "Cabo Verde")
+cl2test <- subset(cl2test, COUNTRY != "Mauritius")
+cl2test <- subset(cl2test, COUNTRY != "Seychelles")
+cl2test <- subset(cl2test, COUNTRY != "São Tomé and Príncipe")
+cl2test <- subset(cl2test, COUNTRY != "Comoros")
+
+
+# Merge shapefile and raster data
+
+cl2testraster <- merge(cl2test,shape,by="GID_2")
+cl2testraster <- st_as_sf(cl2testraster)
+cl2testraster2<-cl2testraster[ , c('COUNTRY.x','NAME_1.x', 'GID_2','NAME_2.x','zonalcat')]
+total3test<-cl2testraster2[!duplicated(cl2testraster2[ , c("GID_2")]),]
+#st_write(total3test, "rainfallreducedclassesworking.shp",append=FALSE)
+st_write(total3test, "Rainfall6clusters.shp",append=FALSE)
+
 
 # Perform K-means clustering with 7 centers
+set.seed(432)
 kmeans_result <- kmeans(km, centers = 7)
 cluster_labels <- kmeans_result$cluster
-
-# Calculate similarity matrix and perform hierarchical clustering
-similarity_matrix <- dist(kmeans_result$centers)
+original_labels <- kmeans_result$cluster
+comparison <- original_labels == cluster_labels
+matching_percentage <- sum(comparison) / length(comparison) * 100
+# Determine similarity matrix using hierarchical clustering
+similarity_matrix <- dist( kmeans_result$centers)
 hc <- hclust(similarity_matrix)
-reordered_labels <- cutree(hc, 7) 
+reordered_labels <- cutree(hc,7) 
 cluster_labels <- reordered_labels[cluster_labels]
-
 # Assign cluster labels to raster
-cl <- raster(files_stack)
+cl = raster(allrasters)
 cl[] = cluster_labels 
-
-# Plot the clustered raster
-plot(cl)
-
-# Mask the raster to Africa shape
-r_cluster <- mask(cl, shape)
+r_cluster <- setValues(cl, cluster_labels)
+plot(r_cluster)
+r_cluster <- mask(r_cluster, shape)
 plot(hc)
 
-# Extract the most common cluster value for each district in Africa
-cl2test <- data.frame(shape, extract(r_cluster, shape, fun = modal, na.rm = TRUE))
 
-# Assign hierarchical clustering classes
+# Extract most common cluster value for each district in Africa
+cl2test<-data.frame(shape,extract(r_cluster, shape, fun=modal, na.rm = TRUE))
+cl2test$zonaltest<-cl2test$extract.r_cluster..shape..fun...modal..na.rm...TRUE.
+plot(hc)
+#labelling zones, into the linked classes based on hc
 cl2test$zonalcat  <- with(cl2test, ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. == 1, 'Class 2', 
                                           ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. == 2, 'Class 4', 
                                                  ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE.== 3, 'Class 6',
@@ -134,50 +141,57 @@ cl2test$zonalcat  <- with(cl2test, ifelse(extract.r_cluster..shape..fun...modal.
                                                                       ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. ==6 , 'Class 3',
                                                                              ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. ==7 , 'Class 7', 
                                                                               0 ))))))))
-# Check for missing values
-sum(is.na(cl2test$extract.r_cluster..shape..fun...modal..na.rm...TRUE.))
-
-# Remove islands
-cl2test <- subset(cl2test, !(COUNTRY %in% c("Cabo Verde", "Mauritius", "Seychelles", "São Tomé and Príncipe", "Comoros")))
-
-# Convert to sf object
-cl2test_missing <- st_as_sf(cl2test)
+# Check for NA values
 sum(is.na(cl2test$extract.r_cluster..shape..fun...modal..na.rm...TRUE.))
 table(cl2test$zonalcat)
 
-# Merge shapefile and aerosol data
-cl2testraster <- merge(cl2test, shape, by = "GID_2")
-cl2testraster2 <- cl2testraster[, c('COUNTRY.x','NAME_1.x', 'GID_2','NAME_2.x','zonalcat')]
-total3test <- cl2testraster2[!duplicated(cl2testraster2[, "GID_2"]), ]
+
+
+# Remove islands
+cl2test <- subset(cl2test, COUNTRY != "Cabo Verde")
+cl2test <- subset(cl2test, COUNTRY != "Mauritius")
+cl2test <- subset(cl2test, COUNTRY != "Seychelles")
+cl2test <- subset(cl2test, COUNTRY != "São Tomé and Príncipe")
+cl2test <- subset(cl2test, COUNTRY != "Comoros")
+
+
+# Merge shapefile and raster data
+
+cl2testraster <- merge(cl2test,shape,by="GID_2")
+cl2testraster <- st_as_sf(cl2testraster)
+cl2testraster2<-cl2testraster[ , c('COUNTRY.x','NAME_1.x', 'GID_2','NAME_2.x','zonalcat')]
+total3test<-cl2testraster2[!duplicated(cl2testraster2[ , c("GID_2")]),]
 st_write(total3test, "rainfall7clusters.shp",append=FALSE)
 
-set.seed(3)
+
+
 
 # Perform K-means clustering with 8 centers
+set.seed(42342)
 kmeans_result <- kmeans(km, centers = 8)
 cluster_labels <- kmeans_result$cluster
-
-# Calculate similarity matrix and perform hierarchical clustering
-similarity_matrix <- dist(kmeans_result$centers)
+original_labels <- kmeans_result$cluster
+comparison <- original_labels == cluster_labels
+matching_percentage <- sum(comparison) / length(comparison) * 100
+# Determine similarity matrix using hierarchical clustering
+similarity_matrix <- dist( kmeans_result$centers)
 hc <- hclust(similarity_matrix)
-reordered_labels <- cutree(hc, 8) 
+reordered_labels <- cutree(hc,8) 
 cluster_labels <- reordered_labels[cluster_labels]
-
 # Assign cluster labels to raster
-cl <- raster(files_stack)
+cl = raster(allrasters)
 cl[] = cluster_labels 
-
-# Plot the clustered raster
-plot(cl)
-
-# Mask the raster to Africa shape
-r_cluster <- mask(cl, shape)
+plot(hc)
+r_cluster <- setValues(cl, cluster_labels)
+plot(r_cluster)
+r_cluster <- mask(r_cluster, shape)
 plot(hc)
 
-# Extract the most common cluster value for each district in Africa
-cl2test <- data.frame(shape, extract(r_cluster, shape, fun = modal, na.rm = TRUE))
-
-# Assign hierarchical clustering classes
+# Extract most common cluster value for each district in Africa
+cl2test<-data.frame(shape,extract(r_cluster, shape, fun=modal, na.rm = TRUE))
+cl2test$zonaltest<-cl2test$extract.r_cluster..shape..fun...modal..na.rm...TRUE.
+plot(hc)
+#labelling zones, into the linked classes based on hc
 cl2test$zonalcat  <- with(cl2test, ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. == 1, 'Class 4', 
                                           ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. == 2, 'Class 7', 
                                                  ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE.== 3, 'Class 1',
@@ -186,48 +200,47 @@ cl2test$zonalcat  <- with(cl2test, ifelse(extract.r_cluster..shape..fun...modal.
                                                                       ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. ==6 , 'Class 8',
                                                                              ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. ==7 , 'Class 3',
                                                                                     ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. ==8 , 'Class 6',0 )))))))))
-# Check for missing values
-sum(is.na(cl2test$extract.r_cluster..shape..fun...modal..na.rm...TRUE.))
 
 # Remove islands
-cl2test <- subset(cl2test, !(COUNTRY %in% c("Cabo Verde", "Mauritius", "Seychelles", "São Tomé and Príncipe", "Comoros")))
+cl2test <- subset(cl2test, COUNTRY != "Cabo Verde")
+cl2test <- subset(cl2test, COUNTRY != "Mauritius")
+cl2test <- subset(cl2test, COUNTRY != "Seychelles")
+cl2test <- subset(cl2test, COUNTRY != "São Tomé and Príncipe")
+cl2test <- subset(cl2test, COUNTRY != "Comoros")
 
-# Convert to sf object
-cl2test_missing <- st_as_sf(cl2test)
-sum(is.na(cl2test$extract.r_cluster..shape..fun...modal..na.rm...TRUE.))
-table(cl2test$zonalcat)
 
-# Merge shapefile and aerosol data
-cl2testraster <- merge(cl2test, shape, by = "GID_2")
-cl2testraster2 <- cl2testraster[, c('COUNTRY.x','NAME_1.x', 'GID_2','NAME_2.x','zonalcat')]
-total3test <- cl2testraster2[!duplicated(cl2testraster2[, "GID_2"]), ]
+# Merge shapefile and raster data
+
+cl2testraster <- merge(cl2test,shape,by="GID_2")
+cl2testraster <- st_as_sf(cl2testraster)
+cl2testraster2<-cl2testraster[ , c('COUNTRY.x','NAME_1.x', 'GID_2','NAME_2.x','zonalcat')]
+total3test<-cl2testraster2[!duplicated(cl2testraster2[ , c("GID_2")]),]
 st_write(total3test, "rainfall8clusters.shp",append=FALSE)
 
-set.seed(4)
-
-# Perform K-means clustering with 6 centers
+# Perform K-means clustering with 9 centers
+set.seed(222)
 kmeans_result <- kmeans(km, centers = 9)
 cluster_labels <- kmeans_result$cluster
-
-# Calculate similarity matrix and perform hierarchical clustering
-similarity_matrix <- dist(kmeans_result$centers)
+original_labels <- kmeans_result$cluster
+comparison <- original_labels == cluster_labels
+matching_percentage <- sum(comparison) / length(comparison) * 100
+# Determine similarity matrix using hierarchical clustering
+similarity_matrix <- dist( kmeans_result$centers)
 hc <- hclust(similarity_matrix)
-reordered_labels <- cutree(hc, 9) 
+reordered_labels <- cutree(hc,9) 
 cluster_labels <- reordered_labels[cluster_labels]
-
 # Assign cluster labels to raster
-cl <- raster(files_stack)
+cl = raster(allrasters)
 cl[] = cluster_labels 
-
-# Plot the clustered raster
-plot(cl)
-
-# Mask the raster to Africa shape
-r_cluster <- mask(cl, shape)
 plot(hc)
+r_cluster <- setValues(cl, cluster_labels)
+plot(r_cluster)
 
-# Extract the most common cluster value for each district in Africa
-cl2test <- data.frame(shape, extract(r_cluster, shape, fun = modal, na.rm = TRUE))
+# Extract most common cluster value for each district in Africa
+cl2test<-data.frame(shape,extract(r_cluster, shape, fun=modal, na.rm = TRUE))
+cl2test$zonaltest<-cl2test$extract.r_cluster..shape..fun...modal..na.rm...TRUE.
+plot(hc)
+#labelling zones, into the linked classes based on hc
 cl2test$zonalcat  <- with(cl2test, ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. == 1, 'Class 8', 
                                           ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. == 2, 'Class 1', 
                                                  ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE.== 3, 'Class 2',
@@ -237,21 +250,20 @@ cl2test$zonalcat  <- with(cl2test, ifelse(extract.r_cluster..shape..fun...modal.
                                                                              ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. ==7 , 'Class 9',
                                                                                     ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. ==8 , 'Class 4',
                                                                                            ifelse(extract.r_cluster..shape..fun...modal..na.rm...TRUE. ==9 , 'Class 5', 0 ))))))))))
-# Check for missing values
-sum(is.na(cl2test$extract.r_cluster..shape..fun...modal..na.rm...TRUE.))
-
 # Remove islands
-cl2test <- subset(cl2test, !(COUNTRY %in% c("Cabo Verde", "Mauritius", "Seychelles", "São Tomé and Príncipe", "Comoros")))
+cl2test <- subset(cl2test, COUNTRY != "Cabo Verde")
+cl2test <- subset(cl2test, COUNTRY != "Mauritius")
+cl2test <- subset(cl2test, COUNTRY != "Seychelles")
+cl2test <- subset(cl2test, COUNTRY != "São Tomé and Príncipe")
+cl2test <- subset(cl2test, COUNTRY != "Comoros")
 
-# Convert to sf object
-cl2test_missing <- st_as_sf(cl2test)
-sum(is.na(cl2test$extract.r_cluster..shape..fun...modal..na.rm...TRUE.))
-table(cl2test$zonalcat)
 
-# Merge shapefile and aerosol data
-cl2testraster <- merge(cl2test, shape, by = "GID_2")
-cl2testraster2 <- cl2testraster[, c('COUNTRY.x','NAME_1.x', 'GID_2','NAME_2.x','zonalcat')]
-total3test <- cl2testraster2[!duplicated(cl2testraster2[, "GID_2"]), ]
+# Merge shapefile and raster data
+
+cl2testraster <- merge(cl2test,shape,by="GID_2")
+cl2testraster <- st_as_sf(cl2testraster)
+cl2testraster2<-cl2testraster[ , c('COUNTRY.x','NAME_1.x', 'GID_2','NAME_2.x','zonalcat')]
+total3test<-cl2testraster2[!duplicated(cl2testraster2[ , c("GID_2")]),]
 st_write(total3test, "rainfall9clusters.shp",append=FALSE)
 
 
