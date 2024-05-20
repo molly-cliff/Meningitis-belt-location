ba69e702f4ce256943e443d9f05b682684f2431d
Molly Cliff <108263895+molly-cliff@users.noreply.github.com>
2024-05-20 14:18:43 +0100

    Update Landcover.R

--- 1. Enviromental cluster analysis/Landcover.R
+++ 1. Enviromental cluster analysis/Landcover.R
@@ -1,65 +1,64 @@
-library(dplyr)
+# Set working directory
+setwd("C:/Users/mvc32/OneDrive - University of Cambridge/Documents")
+
+# Load required libraries
 library(raster)
-library(ggplot2)
-library(reshape2)
-library(terra)
-library(tiff)
-library(RStoolbox)
 library(sf)
 library(plyr)
+library(dplyr)
 
-# Set working directory
-setwd("C:/Users/mvc32/OneDrive - University of Cambridge/Documents/landcover")
+# Read raster data
+landcover <- raster("C3S-LC-L4-LCCS-Map-300m-P1Y-2020-v2.1.1.nc")
 
-# Load landcover raster
-landcover2 <- raster("C3S-LC-L4-LCCS-Map-300m-P1Y-2020-v2.1.1.nc")
+# Plot the raster
+plot(landcover)
 
-# Plot landcover raster
-plot(landcover2)
-
-# Load shapefile
-setwd("C:/Users/mvc32/OneDrive - University of Cambridge/Documents")
+# Read shapefile
 shape <- read_sf(dsn = ".", layer = "Shapefile_improved")
 
-# Crop landcover raster to shapefile extent
-landcover2 <- crop(landcover2, shape)
+# Crop raster to the extent of the shapefile
+landcover_cropped <- crop(landcover, shape)
 
-# Create raster with appropriate resolution
-landcover3 <- aggregate(landcover2, fact = 10, fun = mean)
+# Aggregate raster
+landcover_aggregated <- aggregate(landcover_cropped, fact = 5, fun = mean)
 
-# Perform bilinear interpolation to resample the raster to the new resolution
-r_resampled <- resample(landcover2, landcover3, method = "bilinear")
+# Resample raster using bilinear interpolation
+landcover_resampled <- resample(landcover_cropped, landcover_aggregated, method = "bilinear")
 
-raster2_indexed <- st_simplify(shape, preserveTopology = TRUE)
-landcover2 <- mask(r_resampled, raster2_indexed)
+# Fix invalid geometries in shapefile
+shape_valid <- st_make_valid(shape)
 
-# Plot the masked raster
-plot(landcover2)
+# Check validity of fixed geometries
+validity_fixed <- st_is_valid(shape_valid)
 
-# Extract raster values to shapefile and create a dataframe
-cl2test <- data.frame(shape, extract(landcover2, raster2_indexed, fun = modal, na.rm = TRUE))
+# Simplify shapefile geometries
+shape_simplified <- st_simplify(shape_valid, preserveTopology = TRUE)
 
-# Round extracted zones
-cl2test$zonesrounded <- round_any(cl2test$extract.landcover2..raster2_indexed..fun...modal..na.rm...TRUE., 10)
+# Mask raster using simplified shapefile
+landcover_masked <- mask(landcover_resampled, shape_simplified)
 
-# Define breakpoints and category names for land categories
+# Extract raster values for each polygon in the shapefile
+cl2test <- data.frame(shape, extract(landcover_masked, shape_simplified, fun = modal, na.rm = TRUE))
+
+# Round extracted values
+cl2test$zonesrounded <- round_any(cl2test$extract.landcover_masked..shape_simplified..fun...modal..na.rm...TRUE., 10)
+
+# Define breakpoints and category names
 breakpoints <- c(0, 40, 120, 130, 140, 150, 160, 180, 190, 200, 210, 220)
-category_names <- c("Cropland", "Tree cover", "Shrubland", "Grassland", "Lichen and Moss",
-                    "Sparse vegetation", "Flooded areas", "Urban areas", "Bare areas",
-                    "Water bodies", "Permanent snow and ice")
+category_names <- c("Cropland", "Tree cover", "Shrubland", "Grassland", "Lichen and Moss", "Sparse vegetation", "Flooded areas", "Urban areas", "Bare areas", "Water bodies", "Permanent snow and ice")
 
-# Create a new column 'new_land_category' with named land categories
+# Create new column with named land categories
 cl2test$new_land_category <- cut(cl2test$zonesrounded, breaks = breakpoints, labels = category_names, include.lowest = TRUE)
 
-# Merge data with shapefile and create an sf object
-cl2testraster <- merge(cl2test, shape, by = "GID_2")
-cl2testraster <- st_as_sf(cl2testraster)
+# Merge extracted data with original shapefile
+cl2test_merged <- merge(cl2test, shape, by = "GID_2")
+cl2test_sf <- st_as_sf(cl2test_merged)
 
 # Select relevant columns
-cl2testraster2 <- cl2testraster[, c('COUNTRY.x', 'NAME_1.x', 'GID_2', 'NAME_2.x', 'new_land_category')]
+cl2test_final <- cl2test_sf[, c('COUNTRY.x', 'NAME_1.x', 'GID_2', 'NAME_2.x', 'new_land_category')]
 
-# Remove duplicated rows based on 'GID_2'
-total3test <- cl2testraster2[!duplicated(cl2testraster2[, c("GID_2")]), ]
+# Remove duplicated entries
+total3test <- cl2test_final[!duplicated(cl2test_final[, c("GID_2")]),]
 
-# Write the final shapefile
-st_write(total3test, "Landcover.shp", append = FALSE)
+# Write final shapefile
+st_write(total3test, "Landcoverbilinear.shp", append = FALSE)
